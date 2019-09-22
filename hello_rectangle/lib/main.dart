415e4404f80dd10758971f7083f2fbb7a751966d
ashish <ashish@skytesters.com>
2019-09-22 20:32:10 +0530

    add new lines

--- hello_rectangle/lib/main.dart
+++ hello_rectangle/lib/main.dart
@@ -1,51 +1,34 @@
 import 'package:flutter/material.dart';
 
-const _padding = EdgeInsets.all(16.0);
+class HelloRectangle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.greenAccent,
+        height: 400.0,
+        width: 300.0,
+        child: Center(
+          child: Text(
+            'Hello!',
+            style: TextStyle(fontSize: 40.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 void main() {
   runApp(
     MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('this is demo'),
+        ),
         body: HelloRectangle(),
       ),
     ),
   );
 }
-
-class HelloRectangle extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: container,
-
-      // child: Container(
-      //   color: Colors.greenAccent,
-      //   height: 400.0,
-      //   width: 300.0,
-      //   child: Padding(
-      //     padding: _padding,
-      //     child: Center(
-      //       child: Text(
-      //         'Hello!',
-      //         style: TextStyle(fontSize: 40.0),
-      //         textAlign: TextAlign.center,
-      //       ),
-      //     ),
-      //   ),
-      // ),
-    );
-  }
-}
-
-var container = Container(
-    color: Colors.purple,
-    width: 300.0,
-    height: 400.0,
-    child: Center(
-      child: Text(
-        "this is variable",
-        style: TextStyle(color: Colors.white, fontSize: 40.0),
-        textAlign: TextAlign.center,
-      ),
-    ));
